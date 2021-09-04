5748895b1c15a0a9865cd1df4218608045a4524b
Lucas Sousa <ls442336@gmail.com>
2021-09-04 15:03:46 -0300

    Add reader settings

--- app/src/components/ZoomControl.vue
+++ app/src/components/ZoomControl.vue
@@ -1,23 +1,12 @@
 <template>
 	<div class="d-flex align-center zoom-control">
-		<v-btn
-			icon
-		>
-			-
-		</v-btn>
-		<div>
-			<v-text-field
-				hide-details
-				single-line
-				solo-inverted
-				class="zoom-control__level"
-				value="100%" />
-		</div>
-		<v-btn
-			icon
-		>
-			+
-		</v-btn>
+		<v-slider
+			min="25"
+			max="200"
+			step="25"
+			thumb-label
+			ticks
+			></v-slider>
 	</div>
 </template>
 
@@ -25,25 +14,12 @@
 	export default {
 			name: 'ZoomControl',
 			props: {
-			}
+				}
 		};
 </script>
 
 <style>
-	.zoom-control div + div {
-		margin-left: 8px;
-	}
-
-	.zoom-control__level {
-		flex: 0;
-	}
-
-	.zoom-control__level .v-input__control {
-		min-height: 0 !important;
-	}
-
-	.zoom-control__level input {
-		max-width: 42px;
-		text-align: center;
-	}
+.zoom-control {
+	min-width: 200px;
+}
 </style>
--- app/src/views/ReadComic.vue
+++ app/src/views/ReadComic.vue
@@ -2,14 +2,46 @@
 	<v-app>
 		<v-app-bar app dark color="primary" >
 			<v-toolbar-title class="title">Vol 1 - Romance Dawn</v-toolbar-title>
-			<template v-slot:extension>
-				<div class="options">
-						<page-counter />
-						<zoom-control />
-				</div>
-			</template>
+			<v-spacer/>
+				<v-menu offset-y>
+					<template v-slot:activator="{ on, attrs }">
+						<v-btn 
+										icon
+										v-bind="attrs"
+										v-on="on"
+										>
+										<v-icon>mdi-cog</v-icon>
+						</v-btn>
+					</template>
+					<v-list>
+						<v-list-item link>
+							Adjust to height
+						</v-list-item>
+						<v-list-item link>
+							Adjust to width
+						</v-list-item>
+						<v-list-item>
+							<div class="config__item">
+								<span>Zoom</span>
+								<zoom-control/>
+							</div>
+						</v-list-item>
+					</v-list>
+				</v-menu>
 		</v-app-bar>
 		<v-main>
+			<v-toolbar
+				absolute
+				color="primary"
+				dark
+				bottom
+				dense
+				width="100%"
+				>
+				<div class="options">
+					<page-counter />
+				</div>
+			</v-toolbar>
 		</v-main>
 	</v-app>
 </template>
@@ -24,26 +56,26 @@
 			name: 'ReadComic',
 			components: {
 					PageCounter,
-					ZoomControl,
+					ZoomControl
 					// Loading
 				}
 		}
 </script>
 
 <style>
-	.options {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		pointer-events: none;
-	}
+.options {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	pointer-events: none;
+}
 
-	.options * {
-		pointer-events: auto;
-	}
+.options * {
+	pointer-events: auto;
+}
 </style>
